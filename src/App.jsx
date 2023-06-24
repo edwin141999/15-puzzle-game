7d3e00646f0993caa08f2bffc96d1e2fd0533209
edwin141999 <gendevilman@gmail.com>
2023-06-23 23:48:56 -0600

    array move & color change done, just need details

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const NUMBERS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, ""].sort(
+    () => Math.random() - 0.5
+  );
+
+  const ORDER_WINNER = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, ""];
+
+  const [number, setNumber] = useState(NUMBERS);
+
+  useEffect(() => {
+    for (let index = 0; index < number.length; index++) {
+      if (number[index] === ORDER_WINNER[index]) {
+        const id = document.getElementById(index);
+        id.classList.remove("bg-red-500");
+        id.classList.add("bg-green-500");
+      }else{
+        const id = document.getElementById(index);
+        id.classList.remove("bg-green-500");
+        id.classList.add("bg-red-500");
+      }
+    }
+  }, [number, ORDER_WINNER,NUMBERS]);
+
+  const moveTile = (index) => {
+    const tile = number[index];
+    const tileIndex = number.indexOf(tile);
+    const emptyIndex = number.indexOf("");
+
+    const tileRow = Math.floor(tileIndex / 4);
+    const emptyRow = Math.floor(emptyIndex / 4);
+    const tileCol = tileIndex % 4;
+    const emptyCol = emptyIndex % 4;
+
+    const isMoveable =
+      (tileRow === emptyRow && Math.abs(tileCol - emptyCol) === 1) ||
+      (tileCol === emptyCol && Math.abs(tileRow - emptyRow) === 1);
+
+    if (isMoveable) {
+      const newNumbers = [...number];
+      newNumbers[tileIndex] = "";
+      newNumbers[emptyIndex] = tile;
+      setNumber(newNumbers);
+    }
+
+    if (NUMBERS === ORDER_WINNER) {
+      alert("You win!");
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="h-screen bg-slate-500 text-white">
+      <h1 className="flex justify-center text-2xl py-5">15-Puzzle Game</h1>
+      <div className="puzzle">
+        {number.map((cell, index) => (
+          <div
+            key={index}
+            id={index}
+            className="tile"
+            onClick={() => moveTile(index)}
+          >
+            {cell}
+          </div>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
